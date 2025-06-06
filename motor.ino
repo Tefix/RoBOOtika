e1064e1c6f1991058a1c62cf1d5f2ea683c0b9ad
Roman Zaitsev <66490718+Tefix@users.noreply.github.com>
2025-06-06 11:59:20 +0300

    Create motor.ino

---
+++ motor.ino
@@ -0,0 +1,32 @@
+int switchPin = 2; // lüliti 1 
+int motor1Pin1 = 3; // viik 2 (L293D) 
+int motor1Pin2 = 4; // viik 7 (L293D) 
+int enablePin = 9; // viik 1(L293D) 
+
+void setup() 
+{ 
+  // sisendid 
+  pinMode(switchPin, INPUT);
+  //väljundid 
+  pinMode(motor1Pin1, OUTPUT); 
+  pinMode(motor1Pin2, OUTPUT); 
+  pinMode(enablePin, OUTPUT); 
+  // aktiveeri mootor1 
+  digitalWrite(enablePin, HIGH); 
+} 
+
+void loop() 
+{ 
+  // kui lüliti on HIGH, siis liiguta mootorit ühes suunas: 
+  if (digitalRead(switchPin) == HIGH) 
+  { 
+    digitalWrite(motor1Pin1, LOW); // viik 2 (L293D) LOW 
+    digitalWrite(motor1Pin2, HIGH); // viik 7 (L293D) HIGH 
+  } 
+  // kui lüliti on LOW, siis liiguta mootorit teises suunas: 
+  else
+  { 
+    digitalWrite(motor1Pin1, HIGH); // viik 2 (L293D) HIGH 
+    digitalWrite(motor1Pin2, LOW); // viik 7 (L293D) LOW 
+  } 
+} 
